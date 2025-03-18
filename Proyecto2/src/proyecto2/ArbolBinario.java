ea76a12b5f297fc3c0926434f884a61e4f2110b1
FrancoBarra <122551451+FrancoBarra@users.noreply.github.com>
2025-03-18 16:09:35 -0400

    Add files via upload

--- Proyecto2/src/proyecto2/ArbolBinario.java
+++ Proyecto2/src/proyecto2/ArbolBinario.java
@@ -20,7 +20,7 @@ public class ArbolBinario {
         return raiz == null;
     }
 
-    public void add(int data, NodoArbol root) {
+        public NodoArbol add(int data, NodoArbol root) {
         NodoArbol nuevo = new NodoArbol(data);
         if (isEmthy()) {
             this.raiz = nuevo;
@@ -29,19 +29,88 @@ public class ArbolBinario {
             if (data < root.getData()) {
                 if (root.getIzHijo() == null) {
                     root.setIzHijo(nuevo);
+
+
                 } else {
                     add(data, root.getIzHijo());
                 }
 
-            } else {
+
+            } 
+                else {
                 if (root.getDeHijo() == null) {
                     root.setDeHijo(nuevo);
+
+
                 } else {
                     add(data, root.getDeHijo());
                 }
 
             }
         }
+   
+        root.setAltura(this.max(root.getIzHijo().getAltura(), root.getDeHijo().getAltura()));
+
+        int dif = this.diferencia(root);
+
+        if (dif > 1 && data < root.getIzHijo().getData()) {
+            return this.rotar_der(root);
+        }
+
+        if (dif < -1 && data > root.getDeHijo().getData()) {
+            return this.rotar_izq(root);
+        }
+
+        if (dif > 1 && data > root.getIzHijo().getData()) {
+            root.setIzHijo(this.rotar_izq(root.getIzHijo()));
+            return this.rotar_der(root);
+        }
+
+        if (dif < -1 && data < root.getDeHijo().getData()) {
+            root.setDeHijo(this.rotar_der(root.getDeHijo()));
+            return this.rotar_izq(root);
+        }
+        
+        return root;
+    }
+
+    public int max(int a, int b) {
+        if (a > b) {
+            return a;
+        }
+        return b;
+    }
+
+    public NodoArbol rotar_izq(NodoArbol nodo) {
+        NodoArbol aux = nodo.getDeHijo();
+        NodoArbol aux2 = nodo.getIzHijo();
+
+        aux.setIzHijo(nodo);
+        nodo.setDeHijo(aux2);
+
+        nodo.setAltura(1 + max(nodo.getDeHijo().getData(), nodo.getIzHijo().getData()));
+        aux.setAltura(1 + max(aux.getDeHijo().getData(), aux.getIzHijo().getData()));
+        return aux;
+    }
+
+    public NodoArbol rotar_der(NodoArbol nodo) {
+        NodoArbol aux = nodo.getIzHijo();
+        NodoArbol aux2 = nodo.getDeHijo();
+
+        aux.setDeHijo(nodo);
+        nodo.setIzHijo(aux2);
+
+        nodo.setAltura(1 + max(nodo.getDeHijo().getData(), nodo.getIzHijo().getData()));
+        aux.setAltura(1 + max(aux.getDeHijo().getData(), aux.getIzHijo().getData()));
+
+        return aux;
+    }
+
+    public int diferencia(NodoArbol root) {
+        if (root != null) {
+            return (root.getIzHijo().getAltura() - root.getDeHijo().getAltura());
+        }
+        return 0;
     }
 
     public NodoArbol delete(int data, NodoArbol root) {
@@ -73,6 +142,67 @@ public class ArbolBinario {
         } else {
             return minimo(actual.getIzHijo());
         }
+    }
+
+    public String imprimirInOrder(NodoArbol nodo, String cadena) {
+
+        if (nodo != null) {
+            cadena = this.imprimirInOrder(nodo.getIzHijo(), cadena);
+            cadena += nodo.getData() + ", ";
+            cadena = this.imprimirInOrder(nodo.getDeHijo(), cadena);
+
+        }
+        return cadena;
+    }
+
+    public void imprimirPostOrder(NodoArbol nodo) {
+        if (nodo != null) {
+            this.imprimirPostOrder(nodo.getIzHijo());
+            this.imprimirPostOrder(nodo.getDeHijo());
+            System.out.println(nodo.getData());
+
+        }
+
+    }
+
+    public void imprimirPreOrder(NodoArbol nodo) {
+        if (nodo != null) {
+            System.out.println(nodo.getData());
+            this.imprimirPreOrder(nodo.getIzHijo());
+            this.imprimirPreOrder(nodo.getDeHijo());
+
+        }
+
+    }
+
+    public NodoArbol buscarPreorder(int i, NodoArbol nodo) {
+        if (nodo != null) {
+            if (nodo.getData() == i) {
+                return nodo;
+            }
+            nodo = buscarPreorder(i, nodo.getIzHijo());
+
+            if (nodo == null) {
+                nodo = buscarPreorder(i, nodo.getDeHijo());
+            }
+            return nodo;
+        }
+        return null;
+    }
+
+    public NodoArbol buscarIndicePre(int i, NodoArbol nodo) {
+        if (i == 0) {
+            return nodo;
+        }
+        if (nodo != null) {
+            nodo = this.buscarIndicePre(i--, nodo.getIzHijo());
+
+            if (nodo == null) {
+                nodo = this.buscarIndicePre(i--, nodo.getDeHijo());
+            }
+            return nodo;
+        }
+        return null;
     }
 }
 
